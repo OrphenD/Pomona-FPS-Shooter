9fb82822205d78b9bd67228c5187fe3cc92a9ae2
OrphenD <AQuach89@gmail.com>
2016-10-29 13:24:20 -0700

    - added preliminary shooting mechanics

---
+++ Assets/Scripts/BulletScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletScript : MonoBehaviour 
+{
+	public float maxDist = 10000000f;
+	public GameObject decalHitWall;
+	public float floatInFrontofWall = 0.00001f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void Awake()
+	{
+		RaycastHit hit;
+		if (Physics.Raycast (transform.position, transform.forward, hit, maxDist)) {
+			if (decalHitWall && hit.transform.tag == "Level Parts") {
+				Instantiate (decalHitWall, hit.point + (hit.normal * floatInFrontofWall), Quaternion.LookRotation (hit.normal));
+			}
+		}
+		Destroy (gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+	
+	}
+}
---
+++ Assets/Scripts/GunScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GunScript : MonoBehaviour 
+{
+
+	public float fireSpeed = 15.0f;
+	public float waitTilNextFire = 0.0f;
+	public GameObject bullet;
+	public GameObject bulletSpawn;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(Input.GetButton("Fire1"))
+		{
+			if(waitTilNextFire <=0)
+			{
+				if (bullet) {
+					Instantiate (bullet, bulletSpawn.transform.position, bulletSpawn.transform.rotation);
+
+				}
+				waitTilNextFire = 1;
+			}
+		}
+		waitTilNextFire -= Time.deltaTime * fireSpeed;
+	}
+}
